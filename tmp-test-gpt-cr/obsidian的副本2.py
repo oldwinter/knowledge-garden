93dc4aabee6e4d3952c9fbdd24ce3e306cd3a57b
oldwinter <cdd2zju@gmail.com>
2023-08-09 17:42:06 +0800

    aab

--- tmp-test-gpt-cr/obsidian的副本2.py
+++ tmp-test-gpt-cr/obsidian的副本2.py
@@ -28,35 +28,3 @@ embeddings = OpenAIEmbeddings(
     # chunk_size=1
     show_progress_bar=True,
 )
-
-## 4.pinecone初始化
-import pinecone
-import os
-# PINECONE_API_KEY="d0e32935-ca46-4a82-be38-34cc17dbdcce"
-# PINECONE_ENV="gcp-starter"
-
-# initialize pinecone
-
-pinecone.init(
-    api_key=os.getenv('PINECONE_API_KEY'),  # find at app.pinecone.io
-    environment=os.getenv('PINECONE_ENV'),  # next to api key in console
-)
-
-index_name = "ingtube-test"
-
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(
-      name=index_name,
-      metric='cosine',
-      dimension=1536  
-)
-
-
-# 将documents按照每16个元素为一组进行分割
-chunks = [documents[i:i + 16] for i in range(0, len(documents), 16)]
-
-from langchain.vectorstores import Pinecone
-## 5.循环调用Pinecone.from_documents方法，从embedding接口生成数据，同时存储向量数据至pinecone
-for chunk in chunks:
-    Pinecone.from_documents(chunk, embeddings, index_name=index_name)
-
